b487602225f8daaabdf6f2baa73ef62fab937334
Rupa Polipilli <96765255+RupaPolipilliMouri@users.noreply.github.com>
2022-02-17 14:42:09 +0530

    profileManagement

---
+++ Microservices code Example/emp_profile.sql
@@ -0,0 +1,4 @@
+SELECT * FROM mouritech_hibernateexamples.emp_profile;
+use mouritech_hibernateexamples;
+insert into emp_profile values(803462,'Rupa','Vizag');
+insert into emp_profile values(803467,'Deepthi','Vijayawada');
